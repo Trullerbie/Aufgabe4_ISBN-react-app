ea7d0a5f4883a4c4cdbb2fd0966d312cad11763e
Leonie Hein <hein.leonie@gmx.net>
2022-07-29 16:58:18 +0200

    Aufgabe b erledigt

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+const { isbn10Checksum, isbn13Checksum } = require("isbn-check/src/isbn-check");
+
+//input und Button
+<textarea name="ChecktIsbn" id="checkSum"></textarea>;
+Button.addEventListener("click", App);
 
 function App() {
+  const isbn10Checksum = () => {
+    if (isbn10Checksum.length < 10) {
+      isbn13Sum.isbn13Checksum();
+    }
+    if (checkSum == "X") {
+      setIsValid(true);
+      document.getElementById("checkSum").style.color = "green";
+    } else if (checkSum % 11 == 0) {
+      setIsValid(true);
+      document.getElementById("checkSum").style.color = "green";
+    } else {
+      setIsValid(false);
+      document.getElementById("checkSum").style.color = "red";
+    }
+  };
+
+  const isbn13Checksum = () => {
+    if (checkSum.length < 13) {
+      isbn10Sum.isbn10Checksum();
+    }
+    if (checkSum == "X") {
+      setIsValid(true);
+      document.getElementById("checkSum").style.color = "green;";
+    } else if (checkSum % 14 == 0) {
+      setIsValid(true);
+      document.getElementById("checkSum").style.color = "green";
+    } else {
+      setIsValid(false);
+      document.getElementById("checkSum").style.color = "red";
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
